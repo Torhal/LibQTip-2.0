f85245de451853922b7d81fe6dd8ec0000578a58
JD <72483040+DancyNerd@users.noreply.github.com>
2026-01-18 12:09:30 -0500

    Updated HorizontalJustification Type
    Updated forced typing in cell.lua to reflect latest verbiage for HorizontalJustification.

--- LibQTip-2.0/Components/Cell.lua
+++ LibQTip-2.0/Components/Cell.lua
@@ -12,7 +12,7 @@ local TooltipManager = QTip.TooltipManager
 ---@field ColSpan integer The number of columns the cell will span. Defaults to 1.
 ---@field ColumnIndex integer The Column index of Cell.
 ---@field FontString FontString The FontString used to set and display textual values on the Cell.
----@field HorizontalJustification JustifyH Cell-specific justification to use ("CENTER", "LEFT" or "RIGHT"). Defaults to the justification of the Column where the Cell resides.
+---@field HorizontalJustification JustifyHorizontal Cell-specific justification to use ("CENTER", "LEFT" or "RIGHT"). Defaults to the justification of the Column where the Cell resides.
 ---@field LeftPadding integer Pixel padding on the left side of the Cell's value. Defaults to 0.
 ---@field MaxWidth? integer The maximum width (in pixels) of the Cell. If the Cell's value is textual and exceeds this width, it will wrap to a new line. Must not be less than the value of MinWidth.
 ---@field MinWidth? integer The minimum width (in pixels) of the Cell. Must not exceed the value of MaxWidth.

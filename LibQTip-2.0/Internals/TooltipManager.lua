2ef9d642eace3efc9a2b1b3a3d5bc162475ede29
James D. Callahan III <darkenelf@gmail.com>
2026-05-22 11:56:24 -0400

    Revert "TooltipManager.ReleaseColumn: Don't set the column's Tooltip to nil - it will simply be set to a valid Tooltip once re-acquired."
    This reverts commit 06096dad340ab7f0009ecd09e88113f0ffa9e0c3.

--- LibQTip-2.0/Internals/TooltipManager.lua
+++ LibQTip-2.0/Internals/TooltipManager.lua
@@ -414,6 +414,7 @@ function TooltipManager:ReleaseColumn(column)
 
     column.HorizontalJustification = "LEFT"
     column.Index = 0
+    column.Tooltip = nil
     column.Width = 0
 
     ScriptManager:ClearScripts(column)
